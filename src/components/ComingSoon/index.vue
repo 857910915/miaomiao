fa9b80e7756ffec7349f978cb056d0384e24436c
unknown <857910915@qq.com>
2019-10-10 11:46:55 +0800

    接入数据接口

--- src/components/ComingSoon/index.vue
+++ src/components/ComingSoon/index.vue
@@ -2,7 +2,7 @@
   <div class="movie_body">
     <!-- <Loading v-if="isLoading" /> -->
     <ul>
-      <li>
+      <!-- <li>
         <div class="pic_show">
           <img src="/images/1.jpg" />
         </div>
@@ -15,15 +15,15 @@
           <p>2018-11-30上映</p>
         </div>
         <div class="btn_pre">预售</div>
-      </li>
-      <!-- <li v-for="item in comingList" :key="item.id">
+      </li> -->
+      <li v-for="item in comingList" :key="item.id">
           <div class="pic_show" @tap="handleToDetail(item.id)">
             <img :src="item.img | setWH('128.180')" />
           </div>
           <div class="info_list">
             <h2 @tap="handleToDetail(item.id)">
               {{ item.nm }}
-              <img v-if="item.version" src="@/assets/maxs.png" alt />
+              <!-- <img v-if="item.version" src="@/assets/maxs.png" alt /> -->
             </h2>
             <p>
               <span class="person">{{ item.wish }}</span> 人想看
@@ -32,14 +32,48 @@
             <p>{{ item.rt }}上映</p>
           </div>
           <div class="btn_pre">预售</div>
-      </li>-->
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: "ComingSoon"
+  name: "ComingSoon",
+  data(){
+        return {
+            comingList : [],
+            isLoading : true,
+            prevCityId : -1
+        };
+    },
+    // activated(){
+
+    //     var cityId = this.$store.state.city.id;
+    //     if( this.prevCityId === cityId ){ return; }
+    //     this.isLoading = true;
+    //     this.axios.get('/api/movieComingList?cityId='+cityId).then((res)=>{
+    //         var msg = res.data.msg;
+    //         if(msg === 'ok'){
+    //             this.comingList = res.data.data.comingList;
+    //             this.isLoading = false;
+    //             this.prevCityId = cityId;
+    //         }
+    //     })
+    // },
+    mounted(){
+      this.axios.get('/api/movieComingList?cityId=10').then((res)=>{
+            var msg = res.data.msg;
+            if(msg === 'ok'){
+                this.comingList = res.data.data.comingList;
+            }
+        })
+    },
+    methods : {
+        handleToDetail(movieId){
+            this.$router.push('/movie/detail/2/' + movieId);
+        }
+    }
 };
 </script>
 
--- src/components/NowPlaying/index.vue
+++ src/components/NowPlaying/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="movie_body" ref="movie_body">
     <!-- <Loading v-if="isLoading" />
-    <Scroller v-else :handleToScroll="handleToScroll" :handleToTouchEnd="handleToTouchEnd"> -->
-      <ul>
-        <li>
+    <Scroller v-else :handleToScroll="handleToScroll" :handleToTouchEnd="handleToTouchEnd">-->
+    <ul>
+      <!-- <li>
           <div class="pic_show">
             <img src="/images/1.jpg" />
           </div>
@@ -17,34 +17,91 @@
             <p>今天55家影院放映607场</p>
           </div>
           <div class="btn_mall">购票</div>
-        </li>
-        <!-- <li class="pullDown">{{ pullDownMsg }}</li>
-        <li v-for="item in movieList" :key="item.id">
-          <div class="pic_show" @tap="handleToDetail(item.id)">
-            <img :src="item.img | setWH('128.180')" />
-          </div>
-          <p class="info_list">
-            <h2 @tap="handleToDetail(item.id)">
-              {{ item.nm }}
-              <img v-if="item.version" src="@/assets/maxs.png" alt />
-            </h2>
-            <p>
-              观众评
-              <span class="grade">{{ item.sc }}</span>
-            </p>
-            <p>主演: {{ item.star }}</p>
-            <p>{{ item.showInfo }}</p>
+      </li>-->
+      <!-- <li class="pullDown">{{ pullDownMsg }}</li> -->
+      <li v-for="item in movieList" :key="item.id">
+        <div class="pic_show" @tap="handleToDetail(item.id)">
+          <img :src="item.img | setWH('128.180')" />
+        </div>
+        <div class="info_list">
+          <h2 @tap="handleToDetail(item.id)">
+            {{ item.nm }}
+            <!-- <img v-if="item.version" src="@/assets/maxs.png" alt /> -->
+          </h2>
+          <p>
+            观众评
+            <span class="grade">{{ item.sc }}</span>
           </p>
-          <div class="btn_mall">购票</div>
-        </li>-->
-      </ul>
+          <p>主演: {{ item.star }}</p>
+          <p>{{ item.showInfo }}</p>
+        </div>
+        <div class="btn_mall">购票</div>
+      </li>
+    </ul>
     <!-- </Scroller> -->
   </div>
 </template>
 
 <script>
 export default {
-  name: "NowPlaying"
+  name: "NowPlaying",
+  data() {
+    return {
+      movieList: []
+      // ,
+      // pullDownMsg: "",
+      // isLoading: true,
+      // prevCityId: -1
+    };
+  },
+  mounted() {
+    this.axios.get("/api/movieOnInfoList?cityId=11").then(res => {
+      var msg = res.data.msg;
+      if (msg === "ok") {
+        this.movieList = res.data.data.movieList;
+      }
+    });
+  }
+  // activated() {
+  //   var cityId = this.$store.state.city.id;
+  //   if (this.prevCityId === cityId) {
+  //     return;
+  //   }
+  //   this.isLoading = true;
+  //   this.axios.get("/api/movieOnInfoList?cityId=" + cityId).then(res => {
+  //     var msg = res.data.msg;
+  //     if (msg === "ok") {
+  //       this.movieList = res.data.data.movieList;
+  //       this.isLoading = false;
+  //       this.prevCityId = cityId;
+  //     }
+  //   });
+  // },
+  // methods: {
+  //   handleToDetail(movieId) {
+  //     //console.log(movieId);
+  //     this.$router.push("/movie/detail/1/" + movieId);
+  //   },
+  //   handleToScroll(pos) {
+  //     if (pos.y > 30) {
+  //       this.pullDownMsg = "正在更新中";
+  //     }
+  //   },
+  //   handleToTouchEnd(pos) {
+  //     if (pos.y > 30) {
+  //       this.axios.get("/api/movieOnInfoList?cityId=11").then(res => {
+  //         var msg = res.data.msg;
+  //         if (msg === "ok") {
+  //           this.pullDownMsg = "更新成功";
+  //           setTimeout(() => {
+  //             this.movieList = res.data.data.movieList;
+  //             this.pullDownMsg = "";
+  //           }, 1000);
+  //         }
+  //       });
+  //     }
+  //   }
+  // }
 };
 </script>
 
@@ -124,4 +181,4 @@ export default {
   padding: 0;
   border: none;
 }
-</style>>
+</style>
